90f4519b5a40141c3c2b378922936ba91adcebcc
James Sodini <james@sodini.io>
2026-04-20 23:19:53 -0700

    Better stats

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -21,6 +21,10 @@ if test -d "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
     fish_add_path -g "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
 end
 
+# Match the prompt/fastfetch palette in plain `ls` across BSD and GNU variants.
+set -gx LSCOLORS "DxExGxbxCxbxbxBxFxxDxBEx"
+set -gx LS_COLORS "di=38;2;255;145;77:ln=38;2;0;119;194:so=38;2;0;137;123:pi=38;2;224;93;101:ex=38;2;124;179;66:bd=38;2;224;93;101:cd=38;2;224;93;101:su=38;2;255;255;255;48;2;224;93;101:sg=38;2;255;255;255;48;2;0;137;123:tw=38;2;0;0;0;48;2;255;145;77:ow=38;2;255;255;255;48;2;224;93;101:or=38;2;224;93;101;1:mi=38;2;224;93;101;1"
+
 alias ducks 'du -cks * | sort -rn | head -n 10'
 
 function __jim_maybe_run_fastfetch
